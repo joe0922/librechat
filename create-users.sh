51535be111ec3d9fd902ce4e5b19811725f7bdec
joe0922 <32444078+joe0922@users.noreply.github.com>
2026-08-10 08:26:53 +0900

    Add script to create multiple users with npm

---
+++ create-users.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+npm run create-user cm50000787@gmail.com cm50000787@gmail.com cm50000787@gmail.com make19782023 --email-verified=True
+npm run create-user cm50000966@gmail.com cm50000966@gmail.com cm50000966@gmail.com make19782023 --email-verified=True
+npm run create-user cm50002067@gmail.com cm50002067@gmail.com cm50002067@gmail.com make19782023 --email-verified=True
+npm run create-user sh20230101@gmail.com sh20230101@gmail.com sh20230101@gmail.com make19782023 --email-verified=True
+npm run create-user cm50001417@gmail.com cm50001417@gmail.com cm50001417@gmail.com make19782023 --email-verified=True
+npm run create-user cm50002074@gmail.com cm50002074@gmail.com cm50002074@gmail.com make19782023 --email-verified=True
+npm run create-user cm50002238@gmail.com cm50002238@gmail.com cm50002238@gmail.com make19782023 --email-verified=True
+npm run create-user cm50001443@gmail.com cm50001443@gmail.com cm50001443@gmail.com make19782023 --email-verified=True
+npm run create-user cm50000398@gmail.com cm50000398@gmail.com cm50000398@gmail.com make19782023 --email-verified=True
